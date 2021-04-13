502bdbd383ecb68d51cebe772a7bc5f88664916b
krogoth112 <combatv@naver.com>
2021-04-13 11:52:11 +0900

    intial commit

--- src/main/java/com/se2/raonsecure/RaonsecureApplication.java
+++ src/main/java/com/se2/raonsecure/RaonsecureApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RaonsecureApplication {
     // 테스트용 주석
+    //11111111
     public static void main(String[] args) {
         SpringApplication.run(RaonsecureApplication.class, args);
     }
