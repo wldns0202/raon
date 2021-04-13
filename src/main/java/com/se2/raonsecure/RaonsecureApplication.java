8a6780e990fa1e71778aecd15f07a3de51b7d61a
JWJEONG <wldns0202@naver.com>
2021-04-13 16:35:13 +0900

    wldns2200 커밋2

--- src/main/java/com/se2/raonsecure/RaonsecureApplication.java
+++ src/main/java/com/se2/raonsecure/RaonsecureApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RaonsecureApplication {
-    //testest
+    //하나둘셋
     public static void main(String[] args) {
         SpringApplication.run(RaonsecureApplication.class, args);
     }
