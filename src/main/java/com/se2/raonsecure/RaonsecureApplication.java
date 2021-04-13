68e135d4a3e9c6f24926dd173f62567ba3e113c8
JWJEONG <wldns0202@naver.com>
2021-04-13 13:24:28 +0900

    테스트용 주석12

--- src/main/java/com/se2/raonsecure/RaonsecureApplication.java
+++ src/main/java/com/se2/raonsecure/RaonsecureApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RaonsecureApplication {
-    // brnch Test
+    // brnch Test 1
     public static void main(String[] args) {
         SpringApplication.run(RaonsecureApplication.class, args);
     }
