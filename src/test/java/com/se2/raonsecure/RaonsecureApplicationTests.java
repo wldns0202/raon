7a89b2362ad9d3b092566ce857f63bae3fb84192
zpblue <56902957+Gangkk@users.noreply.github.com>
2021-04-13 16:15:25 +0900

    Update RaonsecureApplicationTests.java

--- src/test/java/com/se2/raonsecure/RaonsecureApplicationTests.java
+++ src/test/java/com/se2/raonsecure/RaonsecureApplicationTests.java
@@ -2,7 +2,7 @@ package com.se2.raonsecure;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//TestGang_16:15
 @SpringBootTest
 class RaonsecureApplicationTests {
 
