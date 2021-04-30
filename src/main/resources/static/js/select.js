dc55eaf461b9e884db8b57dbab9c025760e31bea
JWJEONG <jwjeong@raoncorp.com>
2021-04-30 17:59:22 +0900

    제품, 고객사 클릭한거 부모페이지 전달 완료

--- src/main/resources/static/js/select.js
+++ src/main/resources/static/js/select.js
@@ -8,10 +8,16 @@ const main = {
     check: function () {
         const radioList = document.getElementsByName('product')
         radioList.forEach((node) => {
-            if(node.checked){
+            if(node.checked)
                 opener.document.getElementById('product').innerText = node.value
-            }
         })
+        const checkList = document.getElementsByName('check')
+        let checkResult = ''
+        checkList.forEach((node) => {
+            if(node.checked)
+                checkResult += node.value + ' '
+        })
+        opener.document.getElementById('check').innerText = checkResult
         window.close()
     }
 };
